79d1252ba0d72958303f21d0e4d440925af3069b
grantholly <gdholly@gmail.com>
2015-06-26 15:56:12 -0700

    adding TODO notes for stream testing

--- stream_test.py
+++ stream_test.py
@@ -1,1 +1,9 @@
 import count_sketch_min
+
+TODO = """
+1. import a log file and iterate sending the lines to a cms instance
+2. stream messages from nc to a cms instance
+  - this will require the creation of a network interface
+3. stream messages from activeMQ into cms
+  - like before, this will require the creation of an interface
+"""
